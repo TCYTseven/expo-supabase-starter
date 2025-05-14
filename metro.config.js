09c272df336ef959a9414707ab1f56c2447d496b
Tejas Chakrapani <tejaschak2008@gmail.com>
2025-05-13 21:14:26 -0400

    bug fixes

--- metro.config.js
+++ metro.config.js
@@ -4,4 +4,7 @@ const { withNativeWind } = require("nativewind/metro");
 // eslint-disable-next-line no-undef
 const config = getDefaultConfig(__dirname);
 
+// Disable package exports
+config.resolver.unstable_enablePackageExports = false;
+
 module.exports = withNativeWind(config, { input: "./global.css" });
